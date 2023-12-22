13f4fe4025ef9f67309d8bd17bb48e960119ee67
Xavier A <1630719@cegepoutaouais.qc.ca>
2023-12-22 11:58:51 -0500

    Presque terminé

--- script.js
+++ script.js
@@ -4,14 +4,76 @@ let $Nom = $("#NomRecette")
 let $Ingredients1 = $("#Ingredients")
 let $Ingredients2 = $("#Ingredients2")
 let $Ingredients3 = $("#Ingredients3")
-let $Ingredients4 = $("#Ingredients4")
-let $Ingredients5 = $("#Ingredients5")
+let $Quantite1 = $("#Quantite")
+let $Quantite2 = $("#Quantite2")
+let $Quantite3 = $("#Quantite3")
+
+let $Instructions = $("#InstructionsRecette")
 
 //Labels d'erreur
 let $LabelNom = $("#ErreurNom")
 let $LabelIngredient1 = $("#ErreurIngredient1")
 let $LabelIngredient2 = $("#ErreurIngredient2")
 let $LabelIngredient3 = $("#ErreurIngredient3")
+
+
+$form.on('submit', function ()
+{
+    $("#RecetteCarte").append('<li class="card col m-2">' +
+        '<div class="card-body">' +
+            '<h2 class="card-title h5">' + $Nom.val()+ '</h2>'+
+            '<div class="card-text">'+
+                '<ul>'+
+                    '<li>' + $Ingredients1.val() + " (" + $Quantite1.val() + ")" + '</li>' +
+                    '<li>' + $Ingredients2.val() + " (" + $Quantite2.val() + ")" +'</li>' +
+                    '<li>' + $Ingredients3.val() + " (" + $Quantite3.val() + ")" + '</li>' +
+                '</ul>' +
+                '<p>' + "Instructions :"  + '<br>' +
+                    $Instructions.val() + '</p>' +
+            '</div>' +
+        '</div>' +
+    '</li>')
+
+
+})
+//Création de 2 recettes
+function CreerRecettes(recette)
+{
+    $("#RecetteCarte").append(`
+     <li class="card col m-2">
+     <div class="card-body">
+        <h2 class="card-title h5">${recette.RecipeName}</h2>
+        <div class="card-text">
+           <ul>
+               <li> ${recette.Ingredient1} (${recette.Quantite1})</li> 
+               <li> ${recette.Ingredient2} (${recette.Quantite2})</li>  
+               <li> ${recette.Ingredient3} (${recette.Quantite3})</li>    
+           </ul>
+           <p> Instructions : <br>
+           ${recette.RecipeInstructions}</p>     
+        </div>
+     </div>
+    </li>`);
+}
+
+
+function afficherToRecettes() {
+    $.getJSON('https://6585b1a6022766bcb8c92bfd.mockapi.io/Recettes/')
+        .done(function (recette) {
+            recette.forEach(function (recette)
+            {
+                CreerRecettes(recette);
+            });
+        })
+        .fail(function (error) {
+            $('.alert').text(error.status).removeClass('d-none');
+        });
+}
+
+//Appeler les fonctions au chargement de la page
+CreerRecettes()
+afficherToRecettes()
+
 
 //Validation nom recette
 $Nom.on('blur', function ()
@@ -63,10 +125,7 @@ $Ingredients3.on('blur', function ()
     }
 })
 
-$form.on('submit', function ()
-{
-    alert("sent")
-})
 
 
-//VALIDER LES INGREDIENTS OPTIONELS SEULEMENT SUR LE CLIC DU BOUTON
+//Création de nouvelles recettes
+
